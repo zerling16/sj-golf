6185180630ce03fdc623c1f57709de431a59e8b4
zerling16 <zerling@nd.edu>
2025-05-14 10:58:46 -0400

    image8

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+import dotenv from "dotenv";
+dotenv.config();
+console.log("Env Loaded Check:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET ? "loaded" : "missing",
+});
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "https://sj-golf.vercel.app",
+    // origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+import workoutRoutes from "./routes/workout.js";
+import userRoutes from "./routes/user.js";
+import imageRoutes from "./routes/image.js";
+
+mongoose
+  .connect(process.env.URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port 4000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/workouts", workoutRoutes);
+
+app.use("/user", userRoutes);
+
+app.use("/image", imageRoutes);
